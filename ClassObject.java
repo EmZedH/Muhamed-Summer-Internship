df3fca27bbf0d8088ab0a49e48a60a4dab6134c0
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-15 15:42:32 +0530

    Add files via upload

---
+++ ClassObject.java
@@ -0,0 +1,12 @@
+public class ClassObject {
+    int x=2;
+    int y=5;
+    int z=x+y;
+}
+class Lala{
+    public static void main(String[] args){
+        ClassObject object = new ClassObject();
+        object.x=5;
+        System.out.println("Sum of "+object.x+" and "+object.y+" is "+object.z);
+    }
+}
