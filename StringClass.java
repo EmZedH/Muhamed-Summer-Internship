6b9af9c9602c0b4b399f65dffd626e70dbec0d09
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-15 17:16:44 +0530

    Add files via upload

---
+++ StringClass.java
@@ -0,0 +1,14 @@
+public class StringClass {
+    public static void main(String[] args){
+        String s = " Hi Muhamed ";
+        String m = new String("How are u?");
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.concat(m));
+        System.out.println(s.trim());
+        System.out.println(s.replace('H','J'));
+        System.out.println(s.substring(5));
+    }
+}
