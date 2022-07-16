4a577e01991983fd528df7f81338e2c1071eb1f0
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-16 12:24:22 +0530

    Add files via upload

---
+++ priorityqueue.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class priorityqueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new PriorityQueue<>();
+        q.add(1);
+        q.add(7);
+        q.add(3);
+        q.add(10);
+        q.add(7);
+        q.remove();
+        Iterator<Integer> it = q.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
