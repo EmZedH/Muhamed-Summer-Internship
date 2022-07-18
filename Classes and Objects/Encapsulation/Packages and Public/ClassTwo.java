6340c0a58aad1a18ab034272d947dbc7d0009800
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-18 17:25:34 +0530

    Update ClassTwo.java

--- Classes and Objects/Encapsulation/Packages and Public/ClassTwo.java
+++ Classes and Objects/Encapsulation/Packages and Public/ClassTwo.java
@@ -1,8 +1,8 @@
-package com.pack2.two;
+package PackageTwo;
 
 public class ClassTwo {
     public static void main(String[] args) {
-        com.pack1.one.classOne obj = new com.pack1.one.classOne();
+        PackageOne.classOne obj = new PackageOne.classOne();
         System.out.println("Value of y in classOne = "+obj.y);
         System.out.println("Value of z in classOne = "+obj.z);
         obj.function();
