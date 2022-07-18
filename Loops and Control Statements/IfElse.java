0e486818b87f36095b66384fb9b05a6c4bb626de
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-18 16:44:00 +0530

    Update IfElse.java

--- Loops and Control Statements/IfElse.java
+++ Loops and Control Statements/IfElse.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class IfElse {
 
     public static void main(String[] args) {
-        int[] a= new int[3];
+        int[] array= new int[3];
         Scanner in = new Scanner(System.in);
         for(int i=0;i<3;i++){
-            a[i]=in.nextInt();
+            array[i]=in.nextInt();
         }
-        if((a[0]<=a[1] && a[0]>=a[2])||(a[0]<=a[2] && a[0]>=a[1])){
-            System.out.println("Second largest number is "+a[0]);
+        if((array[0]<=array[1] && array[0]>=array[2])||(array[0]<=array[2] && array[0]>=array[1])){
+            System.out.println("Second largest number is "+array[0]);
         }
-        else if((a[1]<=a[0] && a[1]>=a[2])||(a[1]<=a[2] && a[1]>=a[0])){
-            System.out.println("Second largest number is "+a[1]);
+        else if((array[1]<=array[0] && array[1]>=array[2])||(array[1]<=array[2] && array[1]>=array[0])){
+            System.out.println("Second largest number is "+array[1]);
         }
         else{
-            System.out.printf("Second largest number is "+a[2]);
+            System.out.printf("Second largest number is "+array[2]);
         }
 
 
