62983b21691a97750d3279b283af2fedd11fe1d6
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-18 17:20:40 +0530

    Update and rename classX.java to classOne.java

---
+++ Classes and Objects/Encapsulation/Packages and Public/classOne.java
@@ -0,0 +1,9 @@
+package com.pack1.one;
+
+public class classOne {
+   public int y = 34;
+   public String z = "Hello";
+   public void function(){
+        System.out.println("Method in classOne executed");
+    }
+}
--- Classes and Objects/Encapsulation/Packages and Public/classX.java
+++
@@ -1,9 +0,0 @@
-package pack1;
-
-public class classX {
-   public int y = 34;
-   public String z = "Hello";
-   public void xfunc(){
-        System.out.println("Method in classX executed");
-    }
-}
