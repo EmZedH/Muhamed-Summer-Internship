3ba5c410bec2eca471a68f5299320ad444868193
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-18 17:39:50 +0530

    Add files via upload

---
+++ String Class/StringPool.java
@@ -0,0 +1,9 @@
+public class StringPool {
+    public static void main(String[] args) {
+        String str1 = "hello";                             //creates in string pool
+        String str2 = new String("hello");                 //creates in heap memory
+        String str3 = new String("hello").intern();        //shifts to string pool
+        System.out.println(str1==str2);                    //false
+        System.out.println(str1==str3);                     //true
+    }
+}
