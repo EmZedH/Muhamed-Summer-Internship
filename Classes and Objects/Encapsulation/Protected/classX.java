dfe82d288e3326673b982b1a0f595d909fc454af
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-18 17:32:45 +0530

    Update classX.java

--- Classes and Objects/Encapsulation/Protected/classX.java
+++ Classes and Objects/Encapsulation/Protected/classX.java
@@ -3,7 +3,7 @@ package PackageOne;
 public class ClassOne {
    protected int y = 34;
    protected String z = "Hello";
-   protected void printFunction(){
+   protected void printFromClassOne(){
         System.out.println("Method in ClassOne executed");
     }
 }
