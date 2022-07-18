7228ed5e40b8f5d38f01c393b93faacd515786c9
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-18 10:58:09 +0530

    Add files via upload

---
+++ Classes and Objects/Encapsulation/pack1/classX.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class classX {
+   public int y = 34;
+   public String z = "Hello";
+   public void xfunc(){
+        System.out.println("Method in classX executed");
+    }
+}
