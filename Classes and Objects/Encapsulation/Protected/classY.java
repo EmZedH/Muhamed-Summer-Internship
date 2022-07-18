bdab7007ddb76a3f181afabe60c383f91f520f61
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-18 17:30:00 +0530

    Update classY.java

--- Classes and Objects/Encapsulation/Protected/classY.java
+++ Classes and Objects/Encapsulation/Protected/classY.java
@@ -1,11 +1,11 @@
-package pack2;
-import pack1.*;
-public class classY extends classX {
+package PackageTwo;
+import PackageOne.*;
+public class ClassTwo extends ClassOne {
     public static void main(String[] args) {
-        classY x = new classY();
-        System.out.println("Value of y in classX = "+x.y);
-        System.out.println("Value of z in classX = "+x.z);
-        x.xfunc();
+        ClassTwo obj = new ClassTwo();
+        System.out.println("Value of y in ClassOne = "+obj.y);
+        System.out.println("Value of z in ClassOne = "+obj.z);
+        obj.printFunction();
 
     }
 }
