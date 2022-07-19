3d78be8708cb5c7aa438b0d25f06942c55f82077
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-19 10:36:36 +0530

    Update HashMapProgram.java

--- Collection Framework/HashMapProgram.java
+++ Collection Framework/HashMapProgram.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class HashMapProgram {
     public static void main(String[] args) {
-        HashMap<Integer,String> hashmap = new HashMap<Integer,String>();
-        hashmap.put(1,"Hello");
-        hashmap.put(3,"How");
-        hashmap.put(5,"Are");
-        hashmap.put(6,"You");
-        for(HashMap.Entry im: hashmap.entrySet()){
+        HashMap<Integer,String> hashMap = new HashMap<Integer,String>();
+        hashMap.put(1,"Hello");
+        hashMap.put(3,"How");
+        hashMap.put(5,"Are");
+        hashMap.put(6,"You");
+        for(HashMap.Entry im: hashMap.entrySet()){
             System.out.println(im.getKey()+" "+im.getValue());
         }
 
