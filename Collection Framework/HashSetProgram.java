7ac1735f6f30e5d0db615ee56c9d064d4c27fa00
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-19 10:37:13 +0530

    Update HashSetProgram.java

--- Collection Framework/HashSetProgram.java
+++ Collection Framework/HashSetProgram.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class HashSetProgram {
     public static void main(String[] args) {
-        HashSet<Integer> hashset = new HashSet<>();
-        hashset.add(0);
-        hashset.add(9);
-        hashset.add(3);
-        hashset.add(11);
-        hashset.add(3);
-        Iterator<Integer> it = hashset.iterator();
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(0);
+        hashSet.add(9);
+        hashSet.add(3);
+        hashSet.add(11);
+        hashSet.add(3);
+        Iterator<Integer> it = hashSet.iterator();
         while(it.hasNext()){
             System.out.println(it.next());
         }
