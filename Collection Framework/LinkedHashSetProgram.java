b59e469981cbdfa4a8cc549356fd9474ac5f3409
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-19 10:37:48 +0530

    Update LinkedHashSetProgram.java

--- Collection Framework/LinkedHashSetProgram.java
+++ Collection Framework/LinkedHashSetProgram.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class LinkedHashSetProgram {
     public static void main(String[] args) {
-        LinkedHashSet<Integer> linkhash = new LinkedHashSet<>();
-        linkhash.add(1);
-        linkhash.add(9);
-        linkhash.add(6);
-        linkhash.add(5);
-        linkhash.add(1);
-        linkhash.remove(9);
-        Iterator<Integer> it = linkhash.iterator();
+        LinkedHashSet<Integer> linkHash = new LinkedHashSet<>();
+        linkHash.add(1);
+        linkHash.add(9);
+        linkHash.add(6);
+        linkHash.add(5);
+        linkHash.add(1);
+        linkHash.remove(9);
+        Iterator<Integer> it = linkHash.iterator();
         while(it.hasNext()){
             System.out.println(it.next());
         }
