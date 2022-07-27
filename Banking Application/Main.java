b5cbc52606a9f1db4be17fa27509b56dae1c8a84
MuhamedZ1 <88522251+MuhamedZ1@users.noreply.github.com>
2022-07-27 15:39:07 +0530

    Add files via upload

--- Banking Application/Main.java
+++ Banking Application/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Account account = new Account(123345, "muhamed",20);
+        System.out.println("Give account number: ");
+        Scanner scannerNumber = new Scanner(System.in);
+        int input = scannerNumber.nextInt();
+
+        System.out.println("Give name: ");
+        Scanner scannerName = new Scanner(System.in);
+        String name = scannerName.nextLine();
+
+        System.out.println("Give balance: ");
+        Scanner scannerBalance = new Scanner(System.in);
+        float balance = scannerBalance.nextFloat();
+
+        Account account = new Account(input, name,balance);
         UserInterface.printAccountDetails(account);
     }
 }
